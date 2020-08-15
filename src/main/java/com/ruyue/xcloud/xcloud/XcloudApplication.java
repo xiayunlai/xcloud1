9a7e31477388866ca1310ba1c8aafbd15846d9b6
11482291B757EBF8F0BD5972B021FC3C <11482291B757EBF8F0BD5972B021FC3C@i-search.com.cn>
2020-08-15 15:14:07 +0800

    Initial commit

---
+++ src/main/java/com/ruyue/xcloud/xcloud/XcloudApplication.java
@@ -0,0 +1,14 @@
+package com.ruyue.xcloud.xcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcloudApplication.class, args);
+    }
+
+}
+`
---
+++ src/test/java/com/ruyue/xcloud/xcloud/XcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruyue.xcloud.xcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
