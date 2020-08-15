8db28e8065d22c858947376aaaffd6c11b338e95
11482291B757EBF8F0BD5972B021FC3C <11482291B757EBF8F0BD5972B021FC3C@i-search.com.cn>
2020-08-15 15:27:39 +0800

    测试提交

--- src/main/java/com/ruyue/xcloud/xcloud/XcloudApplication.java
+++ src/main/java/com/ruyue/xcloud/xcloud/XcloudApplication.java
@@ -2,13 +2,23 @@ package com.ruyue.xcloud.xcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "hello")
 public class XcloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XcloudApplication.class, args);
     }
 
+    @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return String.format("{\"message\":\"Hello %s %s\"}", firstName, lastName);
+    }
+
 }
-`
